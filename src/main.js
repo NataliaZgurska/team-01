f1d215231d989315552c6eda4a5b7f36b07b041a
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-01-28 11:36:28 +0200

    Add-task2

--- src/main.js
+++ src/main.js
@@ -2,22 +2,36 @@
 // При перезавантаженні сторінки, список повинен відображатись одразу
 // Реалізувати функціонал видалення елементів зі списку при натисканні на кнопку видалити
 
+const TASKS_KEY = 'TASKS_KEY';
+
 const form = document.querySelector('#task-form');
 const list = document.querySelector('#task-list');
 
 form.addEventListener('submit', onBtnSubmit);
 
 function onBtnSubmit(e) {
-    e.preventDefault();
-    const inputEl = e.target.taskName.value;
-    console.log(inputEl);    
-    addSingleTask(inputEl);
-    e.target.reset();
+  e.preventDefault();
+  const inputEl = e.target.taskName.value;
+  console.log(inputEl);
+  addSingleTask(inputEl);
+  e.target.reset();
 }
 
+console.log(infoItemLocalStorage() || []);
 
 function addSingleTask(value) {
-    const item = document.createElement("li");
-    item.textContent = value;
-    list.append(item);
+  const arrItem = infoItemLocalStorage() || [];
+  const item = document.createElement('li');
+  item.textContent = value;
+  list.append(item);
+  arrItem.push(value);
+  addItemLocalStorage(TASKS_KEY, arrItem);
+}
+
+function addItemLocalStorage(key, item) {
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+function infoItemLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
 }
