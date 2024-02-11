bde828cd5f70543017b1afde86aed3fd657affa0
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-02-11 11:02:48 +0000

    Deploying to gh-pages from @ NataliaZgurska/team-01@fc5367877c15ad8e94a2eeae13254dafbd3e12cf 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{r,b as a,T as e}from"./assets/restoredata-5795f93e.js";const o=document.querySelector(".ul-local");r(o,e);a(o,e);
+import{r as s,b as e,T as t}from"./assets/restoredata-5795f93e.js";import{a as c}from"./assets/vendor-9b2cd291.js";const r="https://64859281a795d24810b7146a.mockapi.io/work",n="/contacts",l=`${r}${n}`;async function m(){const o=await c.get(l);return console.log(o),o.data}const a=document.querySelector(".ul-local");s(a,t);e(a,t);m();
 //# sourceMappingURL=commonHelpers2.js.map
